17651b0ede1bd5745787b5f37547ae065bd58c90
markjasonespelita <markjasonespelita02@gmail.com>
2026-06-10 11:02:10 -0400

    chore: zyphor whats new debugging

--- whats-new/main.c
+++ whats-new/main.c
@@ -1,52 +1,52 @@
+#include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-#define RELEASE_NOTES "/usr/share/zyphor-whats-new/release-notes.md"
+#define RELEASE_FILE "/usr/share/zyphor-whats-new/release-notes.md"
 
-int main(void)
+static void activate(GtkApplication *app, gpointer user_data)
 {
-    char marker[512];
-    char line[1024];
-    char *home = getenv("HOME");
+    GtkWidget *window;
+    GtkWidget *scrolled;
+    GtkWidget *text_view;
 
-    if (home == NULL)
-        return 1;
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Zyphor What's New");
+    gtk_window_set_default_size(GTK_WINDOW(window), 700, 500);
 
-    snprintf(marker, sizeof(marker), "%s/.zyphor-whats-new", home);
+    scrolled = gtk_scrolled_window_new();
+    gtk_window_set_child(GTK_WINDOW(window), scrolled);
 
-    /* Already shown */
-    if (access(marker, F_OK) == 0)
-        return 0;
+    text_view = gtk_text_view_new();
+    gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(text_view), GTK_WRAP_WORD);
 
-    /* Open release notes */
-    FILE *notes = fopen(RELEASE_NOTES, "r");
+    gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled), text_view);
 
-    if (notes == NULL)
+    FILE *file = fopen(RELEASE_FILE, "r");
+    if (file)
     {
-        fprintf(stderr, "Unable to open %s\n", RELEASE_NOTES);
-        return 1;
+        char buffer[1024];
+        GtkTextBuffer *buffer_text = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+
+        while (fgets(buffer, sizeof(buffer), file))
+        {
+            gtk_text_buffer_insert_at_cursor(buffer_text, buffer, -1);
+        }
+
+        fclose(file);
     }
+}
 
-    printf("====================================\n");
-    printf("      Zyphor OS - What's New\n");
-    printf("====================================\n\n");
+int main(int argc, char **argv)
+{
+    GtkApplication *app;
+    int status;
 
-    while (fgets(line, sizeof(line), notes))
-    {
-        printf("%s", line);
-    }
+    app = gtk_application_new("org.zyphor.whatsnew", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 
-    fclose(notes);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
 
-    /* Create marker */
-    FILE *marker_file = fopen(marker, "w");
-
-    if (marker_file != NULL)
-    {
-        fprintf(marker_file, "seen\n");
-        fclose(marker_file);
-    }
-
-    return 0;
+    return status;
 }
